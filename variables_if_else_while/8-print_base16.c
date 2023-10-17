7bbbe7c7202031b5aeb4a2b67fce5e25d99d5c71
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-17 13:14:14 -0700

    08_HEX

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+
+	return (0);
+}
