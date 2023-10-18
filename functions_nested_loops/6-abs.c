1d199b6ef5a4d17186d3933721e16eb3775cdf28
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-18 13:03:19 -0700

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *print_sign - checks
+ *_abs - checks
  *@n: the integer to check
- *Return: 1 if n greater than zero, 0 if equal, -1 if less than zero
+ *Return: 1 if n greater than zero, 0 if equal, 1 if less than zero
  */
 
 
