699ebe0243ec3c6809a280cbc3ace17e55c208f5
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-19 05:54:14 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return the sum of two int numbers;
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
