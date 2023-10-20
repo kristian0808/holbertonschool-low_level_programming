8b48c060e3f5e872ea2b650c52ee086095cbee8f
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-20 02:34:12 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - function print line
+ * @n: - parameter from input until when print _
+ * main.h header file
+ * Ose 1 / else
+ * Return:0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
