be69b7828251a15a27f1b5077ed05dbcc5df5e6e
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-22 01:58:22 -0700

    0

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -22,3 +22,8 @@ void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
