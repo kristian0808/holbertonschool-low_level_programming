612fb51e689dd40ecfa64a4163716ea21ddb9dcd
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-27 02:59:03 -0700

    06

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_chessboard - pritn a chessboard
+ * @a: pointer to a matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
