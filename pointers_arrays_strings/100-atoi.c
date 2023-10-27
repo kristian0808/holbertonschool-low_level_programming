d881e06989efecc990cc206cd7d16c9576ae0f56
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-27 13:55:12 -0700

    100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+/**
+ * _atoi - function to convert string to number
+ * @s: the string that will be convert
+ * Return: the converted string
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += sign * (s[i] - '0');
+		}
+		else if (result)
+			break;
+		i++;
+	}
+	return (result);
+}
