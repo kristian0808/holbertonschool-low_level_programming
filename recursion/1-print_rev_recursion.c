d70d8b96a3b78a6dcaa74da3317473329c71a198
kristian0808 <kristianlleshaj@hotmail.com>
2023-10-30 04:26:19 -0700

    01

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: String to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
