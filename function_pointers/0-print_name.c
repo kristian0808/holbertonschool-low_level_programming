512b2595a4b6487af384c01d14c3dc37f50aa8aa
kristian0808 <kristianlleshaj@hotmail.com>
2023-11-15 13:20:17 -0800

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
+#include "function_pointers.h"
 /**
- *
- *print_name - Func that takes a string name and function pointer to return nothing
+ *print_name - Func that prints name
  *@name: string
  *@f:pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
