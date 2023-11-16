7fd9032988b6ac3ada0c7ec963b64814d0fc9a65
kristian0808 <kristianlleshaj@hotmail.com>
2023-11-16 03:15:44 -0800

    02

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - check
+ * @size: size
+ * @cmp: pointer
+ * @array: array
+ * @Return: Return or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+					
+						return (i);
+		}
+	}
+	return (-1);
+}
