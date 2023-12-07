2f197c4ca8d86a69790421ca0a7e6a9366ce8376
kristian0808 <kristianlleshaj@hotmail.com>
2023-12-07 00:47:51 -0800

    EX_0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
-	
 
 	if (!ht)
 	{
@@ -19,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t *));
-	
+
 	if (!ht->array)
 	{
 		return (NULL);
